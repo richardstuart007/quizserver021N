4c462374b9cd20ebf99ba3bb103b1f1539279660
Richard <richardstuart007@hotmail.com>
2022-11-17 13:05:49 +1300

    init

---
+++ quizServerConstants.js
@@ -0,0 +1,25 @@
+//
+//  Database (ElephantSQL 20)
+//
+exports.REMOTE_KNEX_CLIENT = 'pg'
+exports.REMOTE_KNEX_PORT = 5432
+exports.REMOTE_KNEX_HOST = 'rosie.db.elephantsql.com'
+exports.REMOTE_KNEX_USER = 'wdscnzxj'
+exports.REMOTE_KNEX_PWD = 'O-7H6IKT6Hhi_xGU7J_rHSBjvbNO218s'
+exports.REMOTE_KNEX_DATABASE = 'wdscnzxj'
+exports.REMOTE_URL_PORT = 9002
+//
+//  Database (Local)
+//
+exports.LOCAL_KNEX_CLIENT = 'pg'
+exports.LOCAL_KNEX_HOST = '127.0.0.1'
+exports.LOCAL_KNEX_USER = 'richa'
+exports.LOCAL_KNEX_PWD = 'london'
+exports.LOCAL_KNEX_DATABASE = 'Quiz021'
+exports.LOCAL_URL_PORT = 8001
+//
+//  URL
+//
+exports.URL_TABLES = '/QuizTables'
+exports.URL_REGISTER = '/QuizRegister'
+exports.URL_SIGNIN = '/QuizSignin'
